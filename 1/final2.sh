d0473fb817f842ea8fa221121439a3019e199dae
Cat-Hanbit <gasida.seo@gmail.com>
2022-06-01 08:11:27 +0900

    install kubernetes

---
+++ 1/final2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -xe
+
+echo "[TASK 10] Install Flannel CNI"
+#kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/gasida/DOIK/main/1/kube-flannel-v0.18.0.yml
+
+echo "sleep 3"
+sleep 3
+
+echo "[TASK 11] Setting PS1"
+kubectl config rename-context "kubernetes-admin@kubernetes" "DOIK-Lab"
+
+echo "[TASK 12] Install Metrics server on k8s-m node - v0.6.1"
+kubectl apply -f https://raw.githubusercontent.com/gasida/DOIK/main/1/metrics-server.yaml
+
+echo "[TASK 13] Dynamically provisioning persistent local storage with Kubernetes on k8s-m node - v0.0.22"
+kubectl apply -f https://raw.githubusercontent.com/gasida/DOIK/main/1/local-path-storage.yaml
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+echo "[TASK 14] NFS External Provisioner on AWS EFS - v4.0.16"
+# https://github.com/kubernetes-sigs/nfs-subdir-external-provisioner
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner
+printf 'tolerations: [{key: node-role.kubernetes.io/master, operator: Exists, effect: NoSchedule}]\n' | \
+  helm install nfs-provisioner -n kube-system nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+  --set nfs.server=$(cat -pP /root/efs.txt) --set nfs.path=/ --set nodeSelector."kubernetes\.io/hostname"=k8s-m \
+  --values /dev/stdin
